a973e1592b5b308703ff118b63bc5994d06c60c9
Kevin <kevin.rozeh2@gmail.com>
2026-01-05 00:12:17 -0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import '@fontsource/outfit/300.css';
+import '@fontsource/outfit/400.css';
+import '@fontsource/outfit/600.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './style.css'
+
+console.log('Bio Link Loaded');
